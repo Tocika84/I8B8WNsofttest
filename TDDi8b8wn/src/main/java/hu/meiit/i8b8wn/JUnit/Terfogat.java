e0625e2f976477eb24568dd4939713f72bf95a6f
Tocika84 <64549517+Tocika84@users.noreply.github.com>
2020-11-28 13:08:04 +0100

    Add files via upload

---
+++ TDDi8b8wn/src/main/java/hu/meiit/i8b8wn/JUnit/Terfogat.java
@@ -0,0 +1,10 @@
+package hu.meiit.i8b8wn.JUnit;
+
+public class Terfogat {
+
+	public int TeglaV(int a, int b, int c) {
+
+		return a * b * c;
+	}
+
+}
---
+++ TDDi8b8wn/src/test/java/hu/meiit/i8b8wn/JUnit/AppTest.java
@@ -0,0 +1,20 @@
+package hu.meiit.i8b8wn.JUnit;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
